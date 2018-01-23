2393de41256a86c1e26442b6855912b270976d33
FreunzWa <insectferocity@gmail.com>
2018-01-23 18:40:15 +1100

    the card container now draws itself

--- CardContainer.py
+++ CardContainer.py
@@ -3,23 +3,48 @@ from definitions import *
 from Card import *
 
 class CardContainer:
-    def __init__(self, size = 0, container_type = "deck"):
+    def __init__(self, size = 0, container_type = "deck", player_no = 1):
         """
         initialises a new card container_type
         the 'top' card is considered to be index 0
         """
-
+        self.player_no = player_no
         self.container_type = container_type
         self.id_values = np.random.randint(1,11,(size,))
         self.contents = np.ndarray([0,])
+        self.bounds = pygame.Surface((0,0))
         for i in self.id_values:
             new_card = Card(card_id = i)
             self.contents = np.append(self.contents, new_card)
 
         print "New card container created of type '%s' with contents: " %(container_type.upper())
         print self.id_values
-        print self.contents
         card_container_list.append(self)
+
+
+    def display(self, target_surface):
+        """
+        The container is drawn to the screen for interactivity.
+        @params:
+        target_surface - the screen or surface for the container to be drawn
+        to
+        """
+        if self.container_type == "deck":
+            card_back = pygame.Surface((card_dimensions))
+            card_back.fill(CARD_OUTLINE)
+            pygame.draw.rect(card_back, CARD_BACK, (1,1,card_back.get_width()-2,card_back.get_height()-2))
+
+            for card_bunch in range(0,len(self.contents)//2):
+                #draws the deck in the appropriate position depending on player
+                max_deck_height =(60)//2*2
+                padding = 4
+                if self.player_no == 1:
+                    target_surface.blit(card_back, (target_surface.get_width()
+                    -card_dimensions[0]-padding, target_surface.get_height()
+                    -card_dimensions[1]-padding-card_bunch*2 ))
+                else:
+                    target_surface.blit(card_back,(padding, padding+max_deck_height-card_bunch*2))
+
 
     def shuffle(self):
         """
@@ -30,7 +55,7 @@ class CardContainer:
         """
         self.contents = np.random.shuffle(self.contents)
 
-    def draw_card(self, target_container):
+    def pull_card(self, target_container):
         """
         @func
         especially used when the container is of type deck. removes the first
