bee94f2f96fd984cce0f66418a417d3c0fd54e1c
FreunzWa <insectferocity@gmail.com>
2018-01-23 14:21:02 +1100

    updating the card class and definitions file for drawing the rest of the card.

--- definitions.py
+++ definitions.py
@@ -1,9 +1,12 @@
 import pygame
 import os
-
+import utilities
 #colors
 WHITE = (255,255,255)
+BLACK = (0,0,0)
 
+CARD_OUTLINE = BLACK
+NORMAL_MONSTER = (244,177,38)
 
 #dir
 path = os.path.dirname(os.path.abspath(__file__))
@@ -11,3 +14,8 @@ path = os.path.dirname(os.path.abspath(__file__))
 
 #objects Lists
 cardList = []
+
+
+#card sizes
+card_dimensions = (127, 192)
+central_padding = 5
